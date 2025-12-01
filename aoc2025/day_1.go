9600262b1afe4a97897a074fe972c2628a2089b3
abeni-al7 <abenezeralebachew3@gmail.com>
2025-12-01 20:52:27 +0300

    Solve day 1 part 1

---
+++ aoc2025/day_1.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	password := ProcessFile("day_1_input.txt")
+	fmt.Println(password)
+}
+
+func ProcessFile(path string) int {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := ScanLineByLine(file)
+	password := ProcessLines(lines)
+	defer file.Close()
+	return password
+}
+
+func ScanLineByLine(file *os.File) []string {
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)	
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
+}
+
+func ProcessLines(lines []string) int {
+	curr := 50
+	pass := 0
+	for _, line := range lines {
+		dir := string(line[0])
+		amount, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if dir == "R" {
+			curr = ((curr + amount) + 100) % 100
+		} else {
+			curr = ((curr - amount) + 100) % 100
+		}
+		if curr == 0 {
+			pass++
+		}
+	}
+	return pass
+}
